a3cc7d529f5fd93ffb94740feed5b3cac77c37ed
IMAZU Asami <neko.daisuki.070109@gmail.com>
2012-10-05 18:24:10 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HoresaseBody::Application.config.secret_token = 'd67ddac66968a69fa598d5c2c12e32353e515b2d011f22f172ad7171291a15160302c88489e5742a371ad8ab7e0ce0ec03e20b531d84211337e2452ac0a462ba'
