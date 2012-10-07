1149ce110bc8935859df587659fbf6b50a93519e
IMAZU Asami <neko.daisuki.070109@gmail.com>
2012-10-07 17:18:15 +0900

    Remove unused methods

--- app/controllers/horesases_controller.rb
+++ app/controllers/horesases_controller.rb
@@ -11,33 +11,6 @@ class HoresasesController < ApplicationController
     end
   end
 
-  # GET /horesases/1
-  # GET /horesases/1.json
-  def show
-    @horesase = Horesase.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @horesase }
-    end
-  end
-
-  # GET /horesases/new
-  # GET /horesases/new.json
-  def new
-    @horesase = Horesase.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @horesase }
-    end
-  end
-
-  # GET /horesases/1/edit
-  def edit
-    @horesase = Horesase.find(params[:id])
-  end
-
   # POST /horesases
   # POST /horesases.json
   def create
@@ -55,33 +28,5 @@ class HoresasesController < ApplicationController
       end
     end
 =end
-  end
-
-  # PUT /horesases/1
-  # PUT /horesases/1.json
-  def update
-    @horesase = Horesase.find(params[:id])
-
-    respond_to do |format|
-      if @horesase.update_attributes(params[:horesase])
-        format.html { redirect_to @horesase, notice: 'Horesase was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @horesase.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /horesases/1
-  # DELETE /horesases/1.json
-  def destroy
-    @horesase = Horesase.find(params[:id])
-    @horesase.destroy
-
-    respond_to do |format|
-      format.html { redirect_to horesases_url }
-      format.json { head :no_content }
-    end
   end
 end
